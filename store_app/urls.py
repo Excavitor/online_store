116b33b27523c228322cc0c9c62a9286ad75f620
shad <shad2000abdullah@gmail.com>
2025-05-29 17:20:07 +0600

    CRUD for Products

--- store_app/urls.py
+++ store_app/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('orders/', views.orders, name='orders'),
     path('categories/', views.categories, name='categories'),
     path('products/', views.products, name='products'),
+    path('products/add/', views.add_product, name='add_product'),
+    path('products/edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('products/delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('orderitems/', views.orderitems, name='orderitems'),
 ]
