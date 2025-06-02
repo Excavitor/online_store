e644ae7f25c1fe85b81fe280ce57392d7211b1ca
shad <shad2000abdullah@gmail.com>
2025-06-02 17:03:47 +0600

    CRUD done for all

--- store_app/urls.py
+++ store_app/urls.py
@@ -3,15 +3,30 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+
     path('dashboard/', views.dashboard, name='dashboard'),
+
     path('customers/', views.customers, name='customer'),
+
     path('orders/', views.orders, name='orders'),
+    path('orders/add/', views.add_order, name='add_order'),
+    path('orders/edit/<int:order_id>/', views.edit_order, name='edit_order'),
+    path('orders/delete/<int:order_id>/', views.delete_order, name='delete_order'),
+
     path('categories/', views.categories, name='categories'),
+    path('categories/add/', views.add_category, name='add_category'),
+    path('categories/edit/<int:category_id>/', views.edit_category, name='edit_category'),
+    path('categories/delete/<int:category_id>/', views.delete_category, name='delete_category'),
+
     path('products/', views.products, name='products'),
     path('products/add/', views.add_product, name='add_product'),
     path('products/edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('products/delete/<int:product_id>/', views.delete_product, name='delete_product'),
+
     path('orderitems/', views.orderitems, name='orderitems'),
+    path('orderitems/add/', views.add_order_item, name='add_order_item'),
+    path('orderitems/edit/<int:order_item_id>/', views.edit_order_item, name='edit_order_item'),
+    path('orderitems/delete/<int:order_item_id>/', views.delete_order_item, name='delete_order_item'),
 
     path('users/', views.list_users, name='list_users'),
     path('users/create/', views.create_user, name='create_user'),
